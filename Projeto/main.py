24610895720e58237b4274ff63ff1e25ff8d3f1c
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2025-06-10 14:14:18 -0300

    tudo errado

--- Projeto/main.py
+++ Projeto/main.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 from flask_cors import CORS
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 
 app = Flask(__name__)
 CORS(app)
+app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'
 
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField("Entrar")
+
+# Rotas
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
 
-@app.route("/contatos")
-def contatos():
-    return "Seja bem-vindo ao sistema de contatos!"
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    print(form.validate_on_submit())
+    if form.validate_on_submit():
+        return redirect(url_for("home"))  # Corrigido para "home"
+    return render_template("login.html", form=form)
+
 
 @app.route("/empresa")
 def empresa():
@@ -20,6 +35,9 @@ def empresa():
 def trabalhe_conosco():
     return "Seja bem-vindo ao sistema de trabalhe conosco!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.route("/contatos")
+def contatos():
+    return "Seja bem-vindo ao sistema de contatos!"
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
